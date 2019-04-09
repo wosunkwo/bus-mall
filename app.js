40c36a90b8b2f9283d48049d8d4dfb807b8f909b
williams osunkwo <wosunkwo@gmail.com>
2019-04-09 13:56:38 -0700

    added the chart

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ var uniqueRandomArr = [0,0,0];
 var globalClick = 0;
 var ulEl = document.getElementById('votingResult');
 var votingResultTextDom = document.getElementById('votingResultText');
+var names = [];
+var clicks = [];
+var itemResultChart;
 
 //this is the constructor
 function ItemPic(name, extension){
   this.filePath = 'images/' + name + '.' + extension;
   this.name = name;
   this.views = 0;
-  this.clicks = 0;
+  this.click = 0;
   allItems.push(this);
 
 }
@@ -22,6 +25,13 @@ function ItemPic(name, extension){
 function addItems(){
   for(var i=0; i<listOfItems.length; i++){
     new ItemPic(listOfItems[i][0], listOfItems[i][1]);
+  }
+}
+
+function updateChartArrays(){
+  for(var i=0; i<allItems.length; i++){
+    names[i] = allItems[i].name;
+    clicks[i] = allItems[i].click;
   }
 }
 
@@ -54,14 +64,16 @@ function showRandomItem(){
 function handleItemClick(event){
   for(var i = 0; i<allItems.length; i++){
     if(allItems[i].name === event.target.alt){
-      allItems[i].clicks += 1;
+      allItems[i].click += 1;
     }
   }
   globalClick += 1;
+  updateChartArrays();
   if(globalClick <= 25){
     showRandomItem();
   }else{
     itemPicsDiv.removeEventListener('click', handleItemClick);
+    drawChart();
     displayVotingResult();
   }
 }
@@ -71,10 +83,52 @@ function displayVotingResult(){
   votingResultTextDom.textContent = 'Result:';
   for(var i =0; i<allItems.length; i++){
     var liEl = document.createElement('li');
-    liEl.textContent = allItems[i].name + ': '+ allItems[i].clicks + ' votes';
+    liEl.textContent = allItems[i].name + ': '+ allItems[i].click + ' votes';
     ulEl.appendChild(liEl);
   }
 }
+
+
+//stuffs related to chart
+var data = {
+  labels: names, // an array that stores the names of the items
+  datasets: [{
+    data: clicks, // an array that stores the amount of clicks for each item
+    backgroundColor: [
+      'bisque', 'green','blue','pink','brown','burlywood','navy','lightblue','black','beige',
+      'apricot','azure','champagne','cyan','indigo','jade','magenta','live','pear','taupe'
+    ],
+    hoverBackgroundColor: [
+      'red','red','red','red','red','red','red','red','red','red',
+      'red','red','red','red','red','red','red','red','red','red',
+    ]
+  }]
+};
+
+function drawChart() {
+  var chDom = document.getElementById('votingResultChart').getContext('2d');
+  itemResultChart = new Chart(chDom, {
+    type: 'polarArea',
+    data: data,
+    options: {
+      responsive: false,
+      animation: {
+        duration: 1000,
+        easing: 'easeOutBounce'
+      }
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          max: 10,
+          min: 0,
+          stepSize: 1.0
+        }
+      }]
+    }
+  });
+}
+
 
 addItems();
 showRandomItem();
