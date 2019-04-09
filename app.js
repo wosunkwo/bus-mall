a140c71119019b5db60a89f4b0db7ef9581252b1
williams osunkwo <wosunkwo@gmail.com>
2019-04-08 17:23:32 -0700

    'was able to get the images to show non duplicate

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 
 var allItems = [];
 var listOfItems = [['bag', 'jpg'], ['banana','jpg'], ['bathroom','jpg'], ['boots','jpg'], ['breakfast','jpg'], ['bubblegum','jpg'], ['chair','jpg'], ['cthulhu','jpg'], ['dog-duck','jpg'], ['dragon','jpg'], ['pen','jpg'], ['pet-sweep','jpg'], ['scissors','jpg'], ['shark','jpg'], ['sweep','png'], ['tauntaun','jpg'], ['unicorn','jpg'], ['usb','gif'], ['water-can','jpg'], ['wine-glass','jpg']];
-var itemPics = document.getElementById('imageSection1');
+var itemPics = [document.getElementById('image1'), document.getElementById('image2'), document.getElementById('image3')];
+//var itemPics = document.getElementById('image1');
+//var itemPics2 = document.getElementById('image2');
+//var itemPics3 = document.getElementById('image3');
 
 function ItemPic(name, extension){
   this.filePath = 'images/' + name + '.' + extension;
@@ -19,10 +22,38 @@ function addItems(){
   }
 }
 
-function showRandomItem(){
+function getRandomItem(){
   var random = Math.floor(Math.random() * listOfItems.length);
-  itemPics.src = allItems[random].filePath;
+  //itemPics.src = allItems[random].filePath;
+
+  return random;
+}
+
+function handleItemClick(){
+  var random = getRandomItem();
+  var temp = random;
+  for(var i=0; i<itemPics.length; i++){
+    itemPics[i].src = allItems[random].filePath;
+    itemPics[i].alt = allItems[random].name;
+    allItems[random].views++;
+    console.log(allItems[random].name);
+    random = getRandomItem();
+    if(i === 0){
+      console.log('i got into the first if condition ' + 'value of i is = '+ i);
+      while(itemPics[i].alt === allItems[random].name){
+        console.log('duplicate random number between ' + itemPics[i].alt + ' and ' + allItems[random].name);
+        random = getRandomItem();
+      }
+    }else if (i === 1){
+      console.log('i got into the second if condition ' + 'value of i is = '+ i);
+      while(itemPics[i].alt === allItems[random].name || allItems[temp].name === allItems[random].name){
+        console.log('duplicate random number between ' + itemPics[i].alt + ' and ' + allItems[random].name + ' or '+ allItems[temp].name + ' and '+ allItems[random].name);
+        random = getRandomItem();
+      }
+    }
+  }
 }
 
 addItems();
-showRandomItem();
+handleItemClick();
+//itemPics.addEventListener('click', handleItemClick);
