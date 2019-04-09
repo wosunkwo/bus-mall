f64d42eca3fe316065b8940bf02c4c504eca8a7a
williams osunkwo <wosunkwo@gmail.com>
2019-04-08 18:32:28 -0700

    completed the function for recording clicks

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 'use strict';
-
 var allItems = [];
 var listOfItems = [['bag', 'jpg'], ['banana','jpg'], ['bathroom','jpg'], ['boots','jpg'], ['breakfast','jpg'], ['bubblegum','jpg'], ['chair','jpg'], ['cthulhu','jpg'], ['dog-duck','jpg'], ['dragon','jpg'], ['pen','jpg'], ['pet-sweep','jpg'], ['scissors','jpg'], ['shark','jpg'], ['sweep','png'], ['tauntaun','jpg'], ['unicorn','jpg'], ['usb','gif'], ['water-can','jpg'], ['wine-glass','jpg']];
 var itemPics = [document.getElementById('image1'), document.getElementById('image2'), document.getElementById('image3')];
-//var itemPics = document.getElementById('image1');
-//var itemPics2 = document.getElementById('image2');
-//var itemPics3 = document.getElementById('image3');
+var itemPicsDiv = document.getElementById('imageSection');
 
+//this is the constructor
 function ItemPic(name, extension){
   this.filePath = 'images/' + name + '.' + extension;
   this.name = name;
@@ -16,44 +14,58 @@ function ItemPic(name, extension){
 
 }
 
+//this function adds the items to the constructor
 function addItems(){
   for(var i=0; i<listOfItems.length; i++){
     new ItemPic(listOfItems[i][0], listOfItems[i][1]);
   }
 }
 
+//this function generates a random number
 function getRandomItem(){
   var random = Math.floor(Math.random() * listOfItems.length);
-  //itemPics.src = allItems[random].filePath;
-
   return random;
 }
 
-function handleItemClick(){
+//this fucnction generates 3 random inages to the dom
+function showRandomItem(){
   var random = getRandomItem();
   var temp = random;
   for(var i=0; i<itemPics.length; i++){
     itemPics[i].src = allItems[random].filePath;
     itemPics[i].alt = allItems[random].name;
+    itemPics[i].title = allItems[random].name;
     allItems[random].views++;
     console.log(allItems[random].name);
     random = getRandomItem();
     if(i === 0){
-      console.log('i got into the first if condition ' + 'value of i is = '+ i);
+      // console.log('i got into the first if condition ' + 'value of i is = '+ i);
       while(itemPics[i].alt === allItems[random].name){
-        console.log('duplicate random number between ' + itemPics[i].alt + ' and ' + allItems[random].name);
+        console.log('duplicate random number caught between ' + itemPics[i].alt + ' and ' + allItems[random].name);
         random = getRandomItem();
       }
     }else if (i === 1){
-      console.log('i got into the second if condition ' + 'value of i is = '+ i);
+      // console.log('i got into the second if condition ' + 'value of i is = '+ i);
       while(itemPics[i].alt === allItems[random].name || allItems[temp].name === allItems[random].name){
-        console.log('duplicate random number between ' + itemPics[i].alt + ' and ' + allItems[random].name + ' or '+ allItems[temp].name + ' and '+ allItems[random].name);
+        console.log('duplicate random number caught between ' + itemPics[i].alt + ' and ' + allItems[random].name + ' or '+ allItems[temp].name + ' and '+ allItems[random].name);
         random = getRandomItem();
       }
     }
   }
+  console.log('-------------------');
+}
+
+//this functions calculates the total number of clicks an image gets and calls the function that will generate 3 new images to the dom
+function handleItemClick(event){
+  console.log(event.target.alt);
+  for(var i = 0; i<allItems.length; i++){
+    if(allItems[i].name === event.target.alt){
+      allItems[i].clicks += 1;
+    }
+  }
+  showRandomItem();
 }
 
 addItems();
-handleItemClick();
-//itemPics.addEventListener('click', handleItemClick);
+showRandomItem();
+itemPicsDiv.addEventListener('click', handleItemClick);
